152d8a8d2fbd91ad540093d67b63843cec8c569c
DzmitryPanimash <dv.panimash@gmail.com>
2018-11-04 18:51:11 +0300

    create folder

--- src/mp3_manager/Main.java
+++ src/mp3_manager/Main.java
@@ -2,7 +2,6 @@ package mp3_manager;
 
 public class Main {
     public static void main(String[] args) {
-        //just for checking the second folder
-        //just for checking the second time)
+        //create folder
     }
 }
