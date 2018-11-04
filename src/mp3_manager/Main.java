ac2de3b3995e6025e2375e7722f11014cd688d72
DzmitryPanimash <dv.panimash@gmail.com>
2018-11-04 18:40:24 +0300

    checking of the second folder again

--- src/mp3_manager/Main.java
+++ src/mp3_manager/Main.java
@@ -3,5 +3,6 @@ package mp3_manager;
 public class Main {
     public static void main(String[] args) {
         //just for checking the second folder
+        //just for checking the second time)
     }
 }
